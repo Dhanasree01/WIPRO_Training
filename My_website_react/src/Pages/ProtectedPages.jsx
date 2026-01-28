a6366303ef500f9fcda15e7f162dcb11935c0b29
Dhanasree01 <dhanasree.kavitha@gmail.com>
2026-01-28 17:09:31 +0530

    Delete My_website_react directory

--- My_website_react/src/Pages/ProtectedPages.jsx
+++
@@ -1,8 +0,0 @@
-import withAuth from "../hoc/withAuth";
-import Admin from "./Admin";
-import Employee from "./Employee";
-import Courses from "./Courses";
-
-export const ProtectedAdminPage = withAuth(Admin);
-export const ProtectedEmployeePage = withAuth(Employee);
-export const ProtectedCoursesPage = withAuth(Courses);
