b7105a9d1d283f46f1124fcff69593701701b470
Dhanasree01 <dhanasree.kavitha@gmail.com>
2026-01-28 15:46:38 +0530

    Delete Activity1 directory

--- Activity1/e-commerce_website/src/components/button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ text }) {
-  return (
-    <button className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition">
-      {text}
-    </button>
-  );
-}
