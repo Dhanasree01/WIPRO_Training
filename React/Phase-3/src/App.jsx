0360d5949ecd53fe94156dcc238febeadd715514
Dhanasree01 <dhanasree.kavitha@gmail.com>
2026-01-29 15:37:29 +0530

    Add files via upload

---
+++ React/Phase-3/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import EmployeeForm from './forms/ValidationSchema'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <EmployeeForm/>
+
+    </>
+  )
+}
+
+export default App
---
+++ React/Phase-3/src/forms/ValidationSchema.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Formik , Form, Field , ErrorMessage} from 'formik';
+import Button from '@mui/material/Button';
+import { employeeSchema } from './EmployeeForm';
+
+function EmployeeForm() {
+  return (
+<>
+
+     <h1>Signup</h1>
+     <Formik
+       initialValues={{
+         name: '',
+         email: '',
+         password: '',
+       }}
+       validationSchema={employeeSchema}
+       onSubmit={values => {
+         console.log(values);
+         alert("Form submitted")
+       }}
+     >
+    <Form>
+   <div>
+       <label>Enter Name :</label>
+       <Field name="name"></Field>
+       <ErrorMessage name="name"></ErrorMessage>
+   </div>
+     <div>
+       <label>Enter Email :</label>
+       <Field name="email"></Field>
+       <ErrorMessage name="email"></ErrorMessage>
+   </div>
+     <div>
+       <label>Enter Name :</label>
+       <Field name="password"></Field>
+       <ErrorMessage name="password"></ErrorMessage>
+   </div>
+
+       <Button onClick=""variant="contained" color="success">
+        Success
+      </Button>
+    </Form>
+</Formik></>
+  )
+}
+export default EmployeeForm
